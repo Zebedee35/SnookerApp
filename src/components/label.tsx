9b972aed17544b6f06d2bf33f0c23ea008085c8c
Tayfun Susamcioglu <tsusamcioglu@gmail.com>
2020-12-07 22:37:00 +0300

    Minor Changes

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -1,12 +1,36 @@
 import React, { FC } from 'react'
 import { StyleSheet, TextStyle, Text } from 'react-native'
 
+const styles = StyleSheet.create({
+  regular: {
+
+  },
+  bold: {
+
+  },
+  light:
+  {
+
+  },
+  medium: {
+
+  },
+  header: {
+    color: '#FFFFFF',
+    fontSize: 26,
+    textAlign: 'center',
+    textShadowColor: '#000000',
+    textShadowRadius: 2,
+    textShadowOffset: { width: 1, height: 1 },
+  }
+})
+
 type TLabelProps = {
   style?: TextStyle
   textType?: 'regular' | 'bold' | 'light' | 'medium'
 }
 
-const Box: FC<TLabelProps> = ({ children, style, textType, ...props }) => {
+const Label: FC<TLabelProps> = ({ children, style, textType, ...props }) => {
   let textStyle = {}
   switch (textType) {
     case 'regular':
@@ -31,20 +55,21 @@ const Box: FC<TLabelProps> = ({ children, style, textType, ...props }) => {
   )
 }
 
-const styles = StyleSheet.create({
-  regular: {
 
-  },
-  bold: {
+const LabelHeader: FC<TLabelProps> = ({ children, style, textType, ...props }) => {
+  let textStyle = {}
+  textStyle = styles.header
+  const passedStyles = Array.isArray(style) ? Object.assign({}, ...style) : style
 
-  },
-  light:
-  {
+  return (
+    <Text style={[textStyle, { ...passedStyles }]} {...props} >
+      {children}
+    </Text>
+  )
 
-  },
-  medium: {
+}
 
-  }
-})
 
-export default Box;
+export default Label;
+
+export { LabelHeader }
