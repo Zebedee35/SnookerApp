c249b486dcc4c4405c5239fc85dd7d1bc957412e
Tayfun Susamcioglu <tsusamcioglu@gmail.com>
2020-12-08 23:24:44 +0300

    Player Photo Component Added

---
+++ src/components/player-photo.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+import { StyleSheet, Image, StyleProp, ImageStyle } from 'react-native'
+import Box from './box'
+import Label from './label'
+
+const styles = StyleSheet.create({
+  default: {
+    borderRadius: 999
+  }
+})
+
+type TPlayerPhotoProps = {
+  imgUri?: string
+  style?: StyleProp<ImageStyle>
+}
+
+const PlayerPhoto: FC<TPlayerPhotoProps> = ({ style, imgUri, }) => {
+  return (
+    <Box>
+      {(!imgUri || imgUri == '')
+        ? <Image source={{ uri: 'http://35Coders.com/common/snooker/img/snooker@2x.png' }} style={[styles.default, style]} />
+        : <Image source={{ uri: `${imgUri}` }} style={[styles.default, style]} />}
+    </Box>
+  )
+}
+
+export default PlayerPhoto;
