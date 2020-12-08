78446bbcede7f7fd6fe82b101a0a08a1de297e25
Tayfun Susamcioglu <tsusamcioglu@gmail.com>
2020-12-09 00:28:16 +0300

    LiveScore Page Complated

--- src/screens/Live.tsx
+++ src/screens/Live.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { RefreshControl, SectionList, StatusBar } from 'react-native'
+import { RefreshControl, ScrollView, SectionList, StatusBar } from 'react-native'
 import snkrApi from '../api/snkrApi'
 
 import Box from '../components/box'
@@ -8,9 +8,11 @@ import ScoreListItem from '../components/score-list-item'
 import { IEvent, IRound } from '../types/apiTypes'
 import { HomeProps } from '../types/navTypes'
 import bg_live from '../assets/bg_live.jpg'
+import Label from '../components/label'
 
 function LiveScreen({ route, navigation }: HomeProps) {
   const [rounds, setRounds] = useState<IRound[]>([])
+  const [updateDate, setUpdateDate] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   const [loading, setLoading] = useState(false)
 
@@ -21,11 +23,13 @@ function LiveScreen({ route, navigation }: HomeProps) {
   const getLiveScores = async () => {
     try {
       setLoading(true)
+      setUpdateDate('loading...')
       const response = await snkrApi.get('/live_score', {
         params: {
 
         }
       })
+      setUpdateDate(response.data.latestJobs)
       setRounds(response.data.rounds)
       setLoading(false)
     }
@@ -35,6 +39,11 @@ function LiveScreen({ route, navigation }: HomeProps) {
     }
   }
 
+  useEffect(() => {
+    getLiveScores()
+  }, [])
+
+
   return (
     <Box style={{ flex: 1 }}>
       <StatusBar barStyle='light-content' />
@@ -42,9 +51,25 @@ function LiveScreen({ route, navigation }: HomeProps) {
         <HeaderImage imageUri={bg_live} />
         <HeaderContainer>
           <HeaderTop title='Live Scores' />
-          <HeaderBottom rightText='updataDate' />
+          <HeaderBottom rightText={updateDate} />
         </HeaderContainer>
       </Header>
+
+      {!rounds || rounds.length === 0
+        ? <ScrollView style={{ flex: 1 }}
+          refreshControl={<RefreshControl refreshing={loading} onRefresh={onRefresh} />}>
+          <Box style={{ flex: 1, marginTop: 100, alignItems: 'center', justifyContent: 'center' }}>
+            {/* picture will come here. */}
+            <Label style={{ fontSize: 16 }}>There is no live matches now!</Label>
+          </Box>
+        </ScrollView>
+        : <SectionList
+          sections={rounds}
+          keyExtractor={(item) => item.recId}
+          renderItem={({ item }) => <ScoreListItem item={item} bigSize={true} />}
+          refreshControl={<RefreshControl refreshing={loading} onRefresh={onRefresh} />} />
+
+      }
 
     </Box >
   )
